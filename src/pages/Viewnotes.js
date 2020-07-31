7c23fedac0e77474cdd9a7daa71e9752d3315ef9
sssodahae <qwer6897@nate.com>
2020-07-31 12:28:00 +0900

    200731 Dahae :  notes search Common 추가

--- src/pages/Viewnotes.js
+++ src/pages/Viewnotes.js
@@ -3,51 +3,15 @@ import { Plus, Search, ChevronLeft } from "react-feather";
 import { Link } from "react-router-dom";
 import Note from "../components/common/Note";
 import BreadCrumbs from "../components/common/BreadCrumbs";
+import NoteSearch from "../components/common/NoteSearch";
 
 const Viewnotes = () => {
   return (
     <div>
       <BreadCrumbs breads={["내 독서록"]} />
 
-      <div className="card mb-3">
-        <div className="card-body p-2" style={{ height: "47px" }}>
-          <div className="d-flex align-items-center collapse transition-none blog-toolbar">
-            <button
-              className="btn btn-sm btn-icon mr-2"
-              data-toggle="collapse"
-              data-target=".blog-toolbar"
-            >
-              <ChevronLeft />
-            </button>
-            <span className="input-icon input-icon-sm">
-              <i className="material-icons">search</i>
-              <input
-                type="text"
-                className="form-control form-control-sm bg-gray-200 border-gray-200"
-                placeholder="Search blog"
-              />
-            </span>
-          </div>
-          <div className="d-flex align-items-center collapse transition-none show blog-toolbar">
-            <Link to="/writenote">
-              <button
-                className="btn btn-outline-primary btn-sm has-icon"
-                type="button"
-              >
-                <Plus /> 독서록 쓰기
-              </button>
-            </Link>
-            <button
-              className="btn btn-light btn-sm btn-icon ml-auto mr-1"
-              type="button"
-              data-toggle="collapse"
-              data-target=".blog-toolbar"
-            >
-              <Search />
-            </button>
-          </div>
-        </div>
-      </div>
+      {/* 글등록 및 검색 */}
+      <NoteSearch />
 
       {/* 독서록리스트 */}
       <Note />
