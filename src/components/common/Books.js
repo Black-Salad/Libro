e2573e5eda890c17229e3e22424cf25e0438d79a
Jade Kim <jadek.engr@gmail.com>
2020-08-03 16:28:22 +0900

    200803 jade : 책꽂이 UI 변경

--- src/components/common/Books.js
+++ src/components/common/Books.js
@@ -11,7 +11,7 @@ const Books = (props) => {
     {
       b_id: 2,
       b_state: "finished",
-      b_title: "여행의이유",
+      b_title: "여행의 이유",
       b_img: "/img/book_thumbnail/여행의이유.jpg",
     },
     {
@@ -38,6 +38,42 @@ const Books = (props) => {
       b_title: "글쓰기 특강",
       b_img: "/img/book_thumbnail/글쓰기특강.jpg",
     },
+    {
+      b_id: 7,
+      b_state: "interested",
+      b_title: "아가미",
+      b_img: "/img/book_thumbnail/아가미.jpg",
+    },
+    {
+      b_id: 8,
+      b_state: "reading",
+      b_title: "여행의 이유",
+      b_img: "/img/book_thumbnail/여행의이유.jpg",
+    },
+    {
+      b_id: 9,
+      b_state: "reading",
+      b_title: "파과",
+      b_img: "/img/book_thumbnail/파과.jpg",
+    },
+    {
+      b_id: 10,
+      b_state: "interested",
+      b_title: "돈만 모으는 여자는 위험하다",
+      b_img: "/img/book_thumbnail/돈만모으는여자는위험하다.jpg",
+    },
+    {
+      b_id: 11,
+      b_state: "interested",
+      b_title: "젠더는 해롭다",
+      b_img: "/img/book_thumbnail/젠더는해롭다.jpg",
+    },
+    {
+      b_id: 12,
+      b_state: "finished",
+      b_title: "글쓰기 특강",
+      b_img: "/img/book_thumbnail/글쓰기특강.jpg",
+    },
   ]);
   const [displayBooks, setDisplayBooks] = useState([]);
 
@@ -47,8 +83,18 @@ const Books = (props) => {
   }, []);
 
   const bookList = displayBooks.map((book, index) => (
-    <li key={index} className="list-group-item" style={{ textAlign: "center" }}>
-      <img src={book.b_img} />
+    <li
+      key={index}
+      className="list-group-item col-4 col-xs-4 col-sm-3 col-md-3 col-xl-2 mb-3"
+      style={{
+        textAlign: "center",
+        padding: "10px",
+        background: "none",
+        border: "none",
+      }}
+      // style={{ textAlign: "center", background: "none", border: "none" }}
+    >
+      <img src={book.b_img} style={{ maxWidth: "100%" }} />
       <br />
       <div className="text-secondary" style={{ marginTop: "10px" }}>
         {book.b_title}
@@ -56,6 +102,6 @@ const Books = (props) => {
     </li>
   ));
 
-  return <ul className="list-group list-group-horizontal">{bookList}</ul>;
+  return <ul className="list-group list-group-horizontal row">{bookList}</ul>;
 };
 export default Books;
