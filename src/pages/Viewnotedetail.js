ae1ffb54216ad6cac8f55eb25c10db3a7fbd5cd5
sssodahae <qwer6897@nate.com>
2020-08-05 12:34:37 +0900

    200805 Dahae : BreadCrumbs 값 추가

--- src/pages/Viewnotedetail.js
+++ src/pages/Viewnotedetail.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BreadCrumbs from "../components/common/BreadCrumbs";
 import ViewnoteDetailCom from "../components/common/ViewnoteDetail";
+import { Link } from "react-router-dom";
 
 const ViewnoteDetail = ({ match }) => {
   return (
     <>
-      <BreadCrumbs breads={["내 독서록"]} />
+      <BreadCrumbs
+        breads={[<Link to="/viewnotes">내 독서록</Link>, "독서록 상세"]}
+      />
 
       {/* note contents */}
       <ViewnoteDetailCom noteIDX={match.params.noteIDX} />
