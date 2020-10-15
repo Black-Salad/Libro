8f5f3a20a2ef91aa5a3cbf87e618d7f68cb490bc
Jade Kim <jadek.engr@gmail.com>
2020-10-15 10:25:04 +0900

    201015 jade: 책 검색 UI 개선

--- src/pages/Searchbooks.js
+++ src/pages/Searchbooks.js
@@ -7,19 +7,25 @@ import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 import Pagination from "@material-ui/lab/Pagination";
 import Layout from "../components/Layout";
-
-import { KAKAO_API_URL, KAKAO_API_KEY } from "../constants/config";
+import {
+  KAKAO_API_URL,
+  KAKAO_API_KEY,
+  LIBRO_API_URL,
+} from "../constants/config";
 import BreadCrumbs from "../components/common/BreadCrumbs";
 import Bookprofile from "../components/common/Bookprofile";
 
 const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    cursor: "pointer",
+  },
   searchWin: {
     padding: "2px 4px",
     display: "flex",
     alignItems: "center",
-    width: "75%",
-    height: "40px",
-    marginBottom: "15px",
+    marginBottom: "30px",
   },
   input: {
     marginLeft: theme.spacing(1),
@@ -31,18 +37,37 @@ const useStyles = makeStyles((theme) => ({
   paging: {
     "& > *": {
       marginTop: theme.spacing(2),
-      // margin: "20px auto",
-      // width: "100%",
     },
+  },
+  bImage: {
+    width: "100%",
+    border: "1px solid lightgrey",
+    boxShadow: "2px 2px 4px #999",
+    maxWidth: "120px",
+  },
+  // bTitle: {
+  //   marginTop: 2
+  // },
+  contents: {
+    fontSize: "10px",
+    marginTop: 7,
+    lineHeight: 1.2,
+    overflow: "hidden",
+    maxHeight: "6em",
+    textAligh: "left",
+    // wordWrap: "break-word",
+    display: "-webkit-box",
+    WebkitLineClamp: 3,
+    webkitBoxOrient: "vertical",
   },
 }));
 const scrollToTop = (ref) => window.scrollTo(0, ref.current.offsetTop);
 const Searchbooks = () => {
   const classes = useStyles();
   // 모달 스테이트
-  const [modalState, setModalState] = useState({ open: false });
+  const [modalState, setModalState] = useState(false);
   const onOpenModal = (book) => {
-    setModalState({ open: true });
+    setModalState(true);
     setCurrentBook(book);
   };
   // 현재 선택한 책 정보 저장하는 스테이트
@@ -59,10 +84,11 @@ const Searchbooks = () => {
   const [keyword, setKeyword] = useState("");
   // 검색 결과 저장하는 스테이트
   const [result, setResult] = useState({
-    loading: false, /// ############ 검토 필요
+    loading: false, // ######## 검토 필요
     itemCount: null,
     itemList: [],
   });
+
   const onChangeKeyword = (e) => {
     setKeyword(e.target.value);
   };
@@ -95,18 +121,16 @@ const Searchbooks = () => {
         timeout: 3000,
       })
       .then(({ data }) => {
-        console.log(data.meta);
         setResult({
           loading: true,
           itemCount: data.meta.pageable_count,
           itemList: data.documents,
         });
-
-        setTotalPage(Math.ceil(data.meta.pageable_count / 8));
+        setTotalPage(Math.ceil(data.meta.pageable_count / 8)); // size = 8
         scrollToTop(myRef);
       })
       .catch((err) => {
-        console.error(err); // 에러 표시
+        console.error(err);
         setResult({
           loading: false,
           itemCount: 0,
@@ -118,38 +142,52 @@ const Searchbooks = () => {
   };
 
   const displayList = result.itemList.map((item, index) => (
-    <li
+    <Paper
+      className={classes.paper}
       key={index}
-      className="list-group-item d-flex align-items-center"
       onClick={() => onOpenModal(item)}
     >
       <div className="media">
-        <img src={item.thumbnail} />
-        <div className="media-body ml-2">
-          <h5 className="font-size-sm mb-0">{item.title}</h5>
+        <div className="col-3 col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-1">
+          <img
+            className={classes.bImage}
+            alt=""
+            src={
+              item.thumbnail == ""
+                ? LIBRO_API_URL + "/img/unnamed.png"
+                : item.thumbnail
+            }
+          />
+        </div>
+        <div
+          className={`${classes.bTitle} media-body ml-2 col-9 col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-11`}
+        >
+          <h5 className="font-size-sm mb-0 text-secondary">
+            <b>{item.title}</b>
+          </h5>
           <span className="small text-secondary">
-            {item.authors.join(", ") + " 저"}
+            {item.authors.join(", ") + " / " + item.publisher}
           </span>
-          <div>
-            <span className="badge badge-info"></span>
+          <div className={`small text-secondary ${classes.contents}`}>
+            {/* {item.contents} */}
+            {item.contents !== "" ? item.contents + ".." : null}
           </div>
         </div>
       </div>
-    </li>
+
+      {/* </li> */}
+    </Paper>
   ));
 
   return (
     <Layout>
       <div ref={myRef}>
         <BreadCrumbs breads={["책 검색"]} />
-        {/* <div className="d-flex align-items-center collapse transition-none show blog-toolbar"> */}
-
         {/* 검색창 구역 */}
         <Paper component="form" className={classes.searchWin}>
           <InputBase
             className={classes.input}
             placeholder="제목 / 저자명 / 출판사"
-            // inputProps={{ "aria-label": "search google maps" }}
             name="keyword"
             value={keyword}
             onChange={onChangeKeyword}
@@ -164,21 +202,19 @@ const Searchbooks = () => {
           </IconButton>
         </Paper>
 
-        <ul className="list-group list-group-example">
-          {result.itemCount !== 0 ? (
-            displayList
-          ) : (
-            <li className="list-group-item d-flex align-items-center">
-              검색하신 책이 존재하지 않습니다.
-            </li>
-          )}
-        </ul>
+        {result.itemCount !== 0 ? (
+          displayList
+        ) : (
+          <Paper className={classes.paper}>
+            검색하신 책이 존재하지 않습니다.
+          </Paper>
+        )}
         <Bookprofile
-          open={modalState.open}
+          open={modalState}
           setModalState={setModalState}
           currentBook={currentBook}
         />
-        {result.loading ? (
+        {result.loading && result.itemCount !== 0 ? (
           <div className={classes.paging}>
             <Pagination
               count={totalPage}
